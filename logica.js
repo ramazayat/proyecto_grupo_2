fe1d13eba24487f336c64224864a4f6efc3495ca
Grupo2 <ramiro.zayat@escuelacangallo.edu.ar>
2023-05-24 09:07:08 -0300

    Seguimos con la lógica

--- logica.js
+++ logica.js
@@ -1,9 +1,9 @@
-const bizcocho = [1,1,0,0,0];
-const tortillaPapa = [1,0,0,1,0];
-const galletitasManteca = [1,1,1,0,1];
-const purePapa = [0,0,1,1,1];
+const bizcocho = [0,1,1,0,0,0];
+const tortillaPapa = [0,1,0,0,1,0];
+const galletitasManteca = [0,1,1,1,0,1];
+const purePapa = [0,0,0,1,1,1];
 const listaRecetas = [bizcocho, tortillaPapa, galletitasManteca, purePapa];
-const ej = [0,0,1,1,1];
+const ingredientesUsuario = [0,0,0,1,1,1];
 const cantRecetas = 4;
 const cantIngredientes = 5;
 
@@ -18,17 +18,32 @@ function ingredientesFaltantes (receta, busqueda){
 }
 
 function ordenarRecetas(){
- const ordenar = [];
-
 for(let i=0; i<cantRecetas; i++){
-  ordenar[i]=ingredientesFaltantes(listaRecetas[i],ej);
+  listaRecetas[i][0]=ingredientesFaltantes(listaRecetas[i],ingredientesUsuario);
 } 
 
-  const menorMayor = ordenar.sort((a,b)=>{
+  /* const menorMayor = ordenar.sort((a,b)=>{
     return a-b;
  });
   
-  return menorMayor
+  return menorMayor */
+  const ordenar = [];
+  for(let i=0; i<cantRecetas; i++){
+    var menor = 0;
+    for(let i=0; i<cantRecetas; i++){
+      if(menor>listaRecetas[i][0]){
+        menor=listaRecetas[i][0];
+      }
+    }
+    for(let i=0; i<cantRecetas; i++){
+      var m=0;
+      if(menor==listaRecetas[i][0]){
+        ordenar[m]=listaRecetas[i];
+        m++;
+      }
+    }
+  }
+  return ordenar;
 }
 
 console.log(ordenarRecetas())
