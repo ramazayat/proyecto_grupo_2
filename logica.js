f90c96fd68486e19ccb6d653b26e9e70ed73d36b
Ramiro Zayat <ramirozayat@escuelacangallo.edu.ar>
2023-06-11 23:56:30 -0300

    Seguimos con el FrontEnd

--- logica.js
+++ logica.js
@@ -1,22 +1,21 @@
-const bizcocho = [1,1,0,0,0];
-const tortillaPapa = [1,0,0,1,0];
-const galletitasManteca = [1,1,1,0,1];
-const purePapa = [0,0,1,1,1];
-const listaRecetas = [[bizcocho, 0, "Bizcocho"], [tortillaPapa, 0, "Tortilla de Papa"], [galletitasManteca, 0, "Galletitas de Manteca"], [purePapa, 0, "Puré de Papa"]];
-//const ingredientesUsuario = [1,1,1,0,0];
-var ingredientesUsuario=[]
+const bizcocho = [0,1];
+const tortillaPapa = [0,3];
+const galletitasManteca = [0,1,2,4];
+const purePapa = [2,3,4];
+const listaRecetas = [[bizcocho, 0, "Bizcocho",2], [tortillaPapa, 0, "Tortilla de Papa",2], [galletitasManteca, 0, "Galletitas de Manteca",4], [purePapa, 0, "Puré de Papa",3]];
+var ingredientesUsuario=[];
 
 
 function agregarIngrediente(id){
-	ingredientesUsuarios.push(id)
+	ingredientesUsuario.push(id);
 }
 
 function ingrediente_click(idIngrediente){
-	if($("ing0").is(":checked")){
-		agregarIngrediente(idIngrediente)
+	if($("#"+idIngrediente).is(":checked")){
+		agregarIngrediente(idIngrediente);
 	}
 	else{
-
+	
 	}
 }
 
@@ -25,7 +24,7 @@ function ingrediente_click(idIngrediente){
 function ingredientesFaltantes(receta, busqueda){
 	var cantIngFaltantes=0;
 	for(let i = 0; i<receta.length; i++){
-		if(receta[i]>busqueda[i]){
+		if(receta[i]===busqueda[i]){
 			cantIngFaltantes++
 		}
 	}
@@ -36,31 +35,32 @@ function ordenarRecetas(){
 	// Recorro la lista de recetas y actualizo...
 
 	for(let i=0; i<listaRecetas.length; i++){
-		listaRecetas[i][1]=ingredientesFaltantes(listaRecetas[i][0],ingredientesUsuario);
+		listaRecetas[i][1]= ingredientesFaltantes(listaRecetas[i][0],ingredientesUsuario);
 	} 
 
 	// Ordeno la lista 
 	
-	var anteriorMayor=bizcocho.length+1;
+	var anteriorMayor=6;
 	var posOrdenar=listaRecetas.length-1;
 	const ordenar = [];
 
 	for(let i=0; i<listaRecetas.length; i++){
 		var mayor = 0;
 		for(let i=0; i<listaRecetas.length; i++){
-			if( listaRecetas[i][1]<anteriorMayor && listaRecetas[i][1]>mayor){
-				mayor=listaRecetas[i][1];
+			if( (listaRecetas[i][3] - listaRecetas[i][1])<anteriorMayor && (listaRecetas[i][3] - listaRecetas[i][1])>mayor){
+				mayor= (listaRecetas[i][3] - listaRecetas[i][1]);
 			}
 		}
 		anteriorMayor=mayor;
 		for(let i=0; i<listaRecetas.length; i++){
-			if(mayor==listaRecetas[i][1]){
+			if(mayor==(listaRecetas[i][3] - listaRecetas[i][1])){
 				ordenar[posOrdenar]=listaRecetas[i][2];
 				posOrdenar--;
 			}
 		}
 	}
-	return ordenar.toString();
+	return ordenar;
 }
 
 console.log(ordenarRecetas())
+console.log(ingredientesUsuario);
