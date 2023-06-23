88d98056d117979149d759a5b716fe548e4acda3
Grupo2 <ramiro.zayat@escuelacangallo.edu.ar>
2023-06-23 10:46:04 -0300

    Empezamos con node js

--- public/script.js
+++ public/script.js
@@ -1,23 +1,39 @@
 const submitBtn = document.getElementById('submitBtn');
-const nameInput = document.getElementById('nameInput');
 const outputDiv = document.getElementById('output');
+const ingredientesUsuario=[];
+function ingrediente_click(idIngrediente){
+  // Obtener el checkbox por su id
+  var checkbox = document.getElementById(idIngrediente);
+
+  // Verificar el estado del checkbox
+  if (checkbox.checked) {
+    // Checkbox marcado: agregar elemento al arreglo
+    ingredientesUsuario.push(idIngrediente);
+    console.log(ingredientesUsuario);
+  } else {
+    // Checkbox desmarcado: remover elemento del arreglo
+    var index = ingredientesUsuario.indexOf(idIngrediente);
+    if (index > -1) {
+      ingredientesUsuario.splice(index, 1);
+      console.log(ingredientesUsuario);
+    }
+  }
+}
 
 // Agrega un listener al botón de submit para que cuando se haga click se envíe el nombre ingresado al servidor
 submitBtn.addEventListener('click', () => {
-  const name = nameInput.value; // Obtiene el nombre ingresado en el input
-
-  fetch('/greet', { // Envía el nombre ingresado al servidor mediante una petición POST
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name }) // El nombre se envía en el body de la petición
-  }) // Luego de enviar la petición, se espera a que el servidor responda
-    .then(response => response.json()) // Se convierte la respuesta a JSON
-    .then(data => { // Si la conversión fue exitosa, se obtiene el saludo y se muestra en pantalla
-      outputDiv.textContent = data.greeting;
-    })
-    .catch(error => { // Si hubo un error, se muestra en consola
-      console.error('Error:', error);
-    });
-});
+fetch('/obtenerRecetas', { // Envía el nombre ingresado al servidor mediante una petición POST
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ ordenar }) // El nombre se envía en el body de la petición
+}) // Luego de enviar la petición, se espera a que el servidor responda
+  .then(response => response.json()) // Se convierte la respuesta a JSON
+  .then(data => { // Si la conversión fue exitosa, se obtiene el saludo y se muestra en pantalla
+    outputDiv.textContent = data.ordenar;
+  })
+  .catch(error => { // Si hubo un error, se muestra en consola
+    console.error('Error:', error);
+  });
+})
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000; // Puerto de la APP Web
+const bizcocho = [0,1];
+const tortillaPapa = [0,3];
+const galletitasManteca = [0,1,2,4];
+const purePapa = [2,3,4];
+const listaRecetas = [[bizcocho, 0, "Bizcocho",2], [tortillaPapa, 0, "Tortilla de Papa",2], [galletitasManteca, 0, "Galletitas de Manteca",4], [purePapa, 0, "Puré de Papa",3]];
 
 // Código para importar el módulo 'bd' que maneja la conexión y las consultas a la BBDD (Está comentado para que no falle si no está corriendo el servidor MySQL)
 //const db = require('./../database/db');
@@ -9,6 +14,7 @@ app.use(express.static(__dirname + "/../public")); // Indica que la carpeta 'pub
 app.use(express.json());  // Indica que se usarán datos en formato JSON en las peticiones
 
 // Ruta para obtener el saludo, recibe el nombre en el body de la petición y devuelve el saludo en formato JSON
+
 app.post('/greet', (req, res) => { 
   const { name } = req.body;
 
@@ -38,7 +44,75 @@ app.post('/greet', (req, res) => {
   */
 });
 
+app.post('/obtenerRecetas', (req, res) => { 
+  const { ingredientesUsuario } = req.body;
+  let recetas = ordenarRecetas();
+  res.json({recetas});
+
+
+  // Código para obtener el saludo usando los datos guardados en la BBDD
+  // Ustedes deben reemplazar este código por el suyo, según las necesidades de su aplicación
+  /* 
+  db.returnGreetingFromDB(name)
+
+    .then((greeting) => {
+      if (greeting) {
+        console.log(greeting);
+        res.json({ greeting });
+      } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    })
+
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+  */
+});
 // Inicializa la aplicación web e imprime un mensaje en la consola indicando el puerto de escucha
 app.listen(port, () => { 
   console.log(`Server running on http://localhost:${port}`);
 });
+
+function ingredientesFaltantes(receta, busqueda){
+  var cantIngFaltantes=0;
+  for(let i = 0; i<receta.length; i++){
+    for(let h = 0; h<busqueda.length; h++){
+      if(receta[i]===busqueda[h]){
+        cantIngFaltantes++
+      }
+    }	
+  }
+  return cantIngFaltantes;
+  }
+  
+  function ordenarRecetas(){	// Recorro la lista de recetas y actualizo...
+  
+  for(let i=0; i<listaRecetas.length; i++){
+    listaRecetas[i][1]= ingredientesFaltantes(listaRecetas[i][0],ingredientesUsuario);
+  } 
+  
+  // Ordeno la lista 
+  
+  var anteriorMayor=32;
+  var posOrdenar=listaRecetas.length-1;
+  const ordenar = [];
+  
+  for(let i=0; i<listaRecetas.length; i++){
+    var mayor = 0;
+    for(let i=0; i<listaRecetas.length; i++){
+      if( (listaRecetas[i][3] - listaRecetas[i][1])<anteriorMayor && (listaRecetas[i][3] - listaRecetas[i][1])>mayor){
+        mayor= (listaRecetas[i][3] - listaRecetas[i][1]);
+      }
+    }
+    anteriorMayor=mayor;
+    for(let i=0; i<listaRecetas.length; i++){
+      if(mayor==(listaRecetas[i][3] - listaRecetas[i][1])&&posOrdenar>=0){
+        ordenar[posOrdenar]=listaRecetas[i][2];
+        posOrdenar--;
+      }
+    }
+  }
+  return ordenar;
+  }
